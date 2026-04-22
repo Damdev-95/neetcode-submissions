1a4495cdec8635e5f2c4c266f2da1313c1088942
Sulaiman Olubiyi <71001536+Damdev-95@users.noreply.github.com>
2026-04-23 00:06:21 +0100

    Add: python-counter - submission-0

---
+++ Python Coding Interviews/python-counter/submission-0.py
@@ -0,0 +1,17 @@
+from collections import Counter
+from typing import Counter as CounterType
+
+
+def count_chars(s1: str, s2: str) -> CounterType:
+    return Counter(s1 + s2)
+
+    # Alternative solution:
+    # count = Counter(s1)
+    # count.update(s2)
+    # return count
+  
+
+# do not modify below this line
+print(count_chars("hello", "world"))
+print(count_chars("hello", "worldhello"))
+print(count_chars("areallylongstring", "heyhowisitgoing"))
